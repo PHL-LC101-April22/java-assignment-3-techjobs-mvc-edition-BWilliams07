eb89a81e1c2b8783b098b7ebace7f859e02a545c
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-09-22 00:57:06 -0400

    changes

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -14,42 +14,6 @@ import java.util.Objects;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
-
-/**
- package org.launchcode.techjobs.mvc.controllers;
-
- import org.launchcode.techjobs.mvc.models.Job;
- import org.launchcode.techjobs.mvc.models.JobData;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.*;
-
- import javax.websocket.server.PathParam;
-
- import java.util.ArrayList;
-
- import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-
-
- /**
- package org.launchcode.techjobs.mvc.controllers;
-
- import org.launchcode.techjobs.mvc.models.Job;
- import org.launchcode.techjobs.mvc.models.JobData;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.*;
-
- import javax.websocket.server.PathParam;
-
- import java.util.ArrayList;
-
- import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-
-
- /**
- * Created by LaunchCode
- */
 @Controller
 @RequestMapping("search")
 public class SearchController {
@@ -59,8 +23,6 @@ public class SearchController {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
-
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @PostMapping(value = "results")
     public String displaySearchResults(Model model,
